922f4bcc797afb32a524ff5e5a19e384d9f4c8aa
artemsyvak <artsyvak@gmail.com>
2018-09-19 19:40:38 +0300

    done with data-bindings, string interpolation, continue on 32th lecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // serverCreationStatus = 'No server was created';
+  // serverName = 'Test server';
+  username = '';
+  constructor() {}
+
+  // onCreateServer() {
+  //   this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+  // }
+  // onUpdateServerName(event: Event) {
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
 }
