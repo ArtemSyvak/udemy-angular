a1c0930280d04414ae7ee13c958aa5d7416355a4
artemsyvak <artsyvak@gmail.com>
2018-09-20 19:22:37 +0300

    prepared all required components for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverCreationStatus = 'No server was created';
-  serverName = 'Kali Server';
-  serverCreated = false;
-  servers = ['Ubuntu Server', 'Debian Server'];
-  username = '';
-  logs = [];
   constructor() {}
-  addLog() {
-    this.logs.push(new Date());
-  }
-  onCreateServer() {
-    this.servers.push(this.serverName);
-    this.serverCreated = true;
-    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
-  }
-  // onUpdateServerName(event: Event) {
-  //   this.serverName = (<HTMLInputElement>event.target).value;
-  // }
 }
