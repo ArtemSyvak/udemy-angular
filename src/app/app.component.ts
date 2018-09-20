52667d864d645228574b051f46f7e40885ed0b6d
artemsyvak <artsyvak@gmail.com>
2018-09-20 19:07:24 +0300

    commit before started new app in this course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // serverCreationStatus = 'No server was created';
-  // serverName = 'Test server';
+  serverCreationStatus = 'No server was created';
+  serverName = 'Kali Server';
+  serverCreated = false;
+  servers = ['Ubuntu Server', 'Debian Server'];
   username = '';
+  logs = [];
   constructor() {}
-
-  // onCreateServer() {
-  //   this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
-  // }
+  addLog() {
+    this.logs.push(new Date());
+  }
+  onCreateServer() {
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+  }
   // onUpdateServerName(event: Event) {
   //   this.serverName = (<HTMLInputElement>event.target).value;
   // }
