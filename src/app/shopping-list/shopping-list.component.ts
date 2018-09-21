d4d12712151d6442ec12ce80a8cb4b6faea48247
artemsyvak <artsyvak@gmail.com>
2018-09-21 18:11:02 +0300

    changed several components for Recipe Book app

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomato', 3),
+    new Ingredient('Cucumber', 3),
+    new Ingredient('Onion', 1),
+    new Ingredient('Sausage', 3)
+  ];
   constructor() { }
 
   ngOnInit() {
