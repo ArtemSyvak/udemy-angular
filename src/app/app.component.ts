bd1a881c0fdbeefc167f57b6ad68f8e16ced1a68
artemsyvak <artsyvak@gmail.com>
2018-09-24 18:00:06 +0300

    added recipe-detail component and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedPath = 'recipe';
+  onNavigate(e: string) {
+    this.loadedPath = e;
+  }
   constructor() {}
 }
